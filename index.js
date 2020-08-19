bca2e0afa6e189f8b17be384f23f39c9f46f1b3e
Michael Hettmer <mail@michael-hettmer.de>
2020-08-19 14:26:17 +0200

    feat(frontend): request user ethereum accounts and render addresses

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-if (window.ethereum) {
-  window.web3 = new Web3(window.ethereum);
-  ethereum.autoRefreshOnNetworkChange = false;
-} else {
-  alert(
-    "Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!"
-  );
-}
-
-const requestAccounts = async () => {
-  try {
-    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-    console.log(accounts);
-    return accounts;
-  } catch (error) {
-    console.log(error);
+// get ethereum web3 provider
+const initEthereum = () => {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    ethereum.autoRefreshOnNetworkChange = false;
+  } else {
+    alert(
+      "Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!"
+    );
   }
 };
 
+let accounts = undefined;
+
+/**
+ * request accounts from ethereum provider and render them on the website
+ */
+const requestAccounts = async () => {
+  try {
+    accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    var element = document.getElementById("accounts");
+    element.innerHTML = accounts.reduce(
+      (prev, cur, index) => `${prev}\nAccount ${index + 1}: ${cur}`,
+      ""
+    );
+    return accounts;
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+};
+
+initEthereum();
 requestAccounts();
