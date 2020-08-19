b26212fff5755e721a7d9b7dd71eafffcbb50e4a
Michael Hettmer <mail@michael-hettmer.de>
2020-08-19 13:21:48 +0200

    build: fix truffle gas limit deviation

--- truffle-config.js
+++ truffle-config.js
@@ -4,11 +4,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
+      gas: 6700000,
     },
     ganache: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
+      gas: 6700000,
     },
   },
   compilers: {
